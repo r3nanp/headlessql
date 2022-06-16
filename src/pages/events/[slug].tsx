15d111b78dbdd68eaedb81cc9d410cf9de208145
Renan Pereira <renan.souza.dev@gmail.com>
2022-06-16 14:09:49 -0300

    feat(pages): create slug dynamic page

--- src/pages/events/[slug].tsx
+++ src/pages/events/[slug].tsx
@@ -1,3 +1,42 @@
-export default function Memories() {
-  return <h1>Memory</h1>
+import { GetStaticPaths, GetStaticProps } from 'next'
+
+import { Layout } from '@/components'
+import { client } from '@/lib/graphql'
+import { GET_EVENT_BY_SLUG } from '@/graphql/queries/event'
+import {
+  EventBySlugRequest,
+  EventBySlugVariables
+} from '@/graphql/types/GetEventBySlug'
+
+export default function Memories({ event }: EventBySlugRequest) {
+  if (!event) return <div>haha</div>
+
+  return (
+    <Layout title={event.title}>
+      <div className="flex items-center">{event.title}</div>
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const slug = params?.slug as string
+
+  const { event } = await client.request<
+    EventBySlugRequest,
+    EventBySlugVariables
+  >(GET_EVENT_BY_SLUG, { slug })
+
+  return {
+    props: {
+      event
+    },
+    revalidate: 60 * 60 * 60
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
 }
