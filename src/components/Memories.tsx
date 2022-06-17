99088585613a0ddd37d6239008991bcd4f0b5d49
Renan Pereira <renan.souza.dev@gmail.com>
2022-06-17 20:31:40 -0300

    feat: create Spinner and Memories

---
+++ src/components/Memories.tsx
@@ -0,0 +1,28 @@
+import { fetchMemory } from '@/graphql/queries/memory'
+import { useResourceById } from '@/hooks/useResourceById'
+import { Spinner } from './Spinner'
+
+export const Memories = ({ eventId }: { eventId: string }) => {
+  const { data, isFetching, isError } = useResourceById(eventId, () =>
+    fetchMemory(eventId)
+  )
+
+  if (!data && isFetching) return <Spinner />
+
+  if (isError) return <p>Puff... All the memories are gone! </p>
+
+  return (
+    <section className="">
+      <h3 className="my-2 text-4xl font-bold">Memories</h3>
+
+      {data?.map(memory => (
+        <blockquote
+          className="quote border-l-4 border-neutral-500 bg-neutral-100 p-4 italic text-neutral-600"
+          key={memory.id}
+        >
+          {memory.story} - {memory.name}
+        </blockquote>
+      ))}
+    </section>
+  )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export * from './Layout'
+export * from './Memories'
+export * from './Spinner'
